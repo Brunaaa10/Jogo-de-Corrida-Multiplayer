86e6a0bbf88d22342421133e73b97cff9811c494
brunaaa10 <brunaleticiadiniz1@gmail.com>
2023-01-26 20:54:34 -0300

    Jogo de Corrida - Formulário

---
+++ js/Player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.fuel = 180;
+    this.life = 180;
+    this.rank = 0;
+    this.score = 0;
+  }
+}
